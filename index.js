f517db4cf2cdf46343009d2bd8e2fde58e80a619
T.Watanabe <watanabe@colorfulcompany.co.jp>
2020-03-12 15:08:24 +0900

    exports Config and FirestoreReader and SecretReader

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const Config = require('./src/config.js')
+const FirestoreReader = require('./src/firestore-reader')
+const SecretReader = require('./src/secret-reader')
 
-module.exports = Config
+module.exports = {
+  Config,
+  FirestoreReader,
+  SecretReader
+}
